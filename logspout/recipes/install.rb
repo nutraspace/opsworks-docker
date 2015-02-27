b282f6562352a72b1a910ba1092d6f887bcde5e1
J-Katzen <alto50@gmail.com>
2015-02-26 19:38:26 -0800

    cleanup logspout

--- logspout/recipes/install.rb
+++ logspout/recipes/install.rb
@@ -4,6 +4,26 @@ Chef::Log.info
 
 hostname = "#{node[:opsworks][:stack][:name]}-#{node[:opsworks][:instance][:hostname]}"
 
+bash "docker-logspout-cleanup" do
+    user "root"
+    code <<-EOH
+        if docker ps | grep logspout;
+        then
+            docker stop logspout
+            sleep 3
+            docker rm -f logspout
+        fi
+        if docker ps -a | grep logspout;
+        then
+            docker rm -f logspout
+        fi
+        if docker images | grep progrium/logspout;
+        then
+            docker rmi -f $(docker images | grep -m 1 progrium/logspout | awk {'print $3'})
+        fi
+    EOH
+end
+
 Chef::Log.info('docker-logspout start')
 bash "docker-logspout" do
     user "root"
