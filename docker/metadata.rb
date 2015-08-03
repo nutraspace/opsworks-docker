56160a46fef62d87891c5718c8901d70d69b264e
Frank Grove <fragro@gmail.com>
2015-08-03 14:16:32 -0500

    ad docker recipes

---
+++ docker/metadata.rb
@@ -0,0 +1,13 @@
+name 'docker'
+maintainer 'Brian Flad'
+maintainer_email 'bflad417@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures Docker'
+version '0.40.3'
+
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'redhat'
+supports 'ubuntu'
